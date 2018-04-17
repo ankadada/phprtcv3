0d37ee7822d69c8e231d8a9f66412b01ffe1f207
anka <425732469@qq.com>
2018-04-17 11:30:52 +0800

    add Comments

--- example/example.php
+++ example/example.php
@@ -12,30 +12,30 @@ $title = 'lfxl';
 try {
     //创建app
     $resp = $client->createApp($hub, $title, $maxUsers);
-    print_r($resp);exit;
+    print_r($resp);
     //获取app状态
-    // $resp = $client->getApp('deq02uhb6');
-    // print_r($resp);exit;
+    $resp = $client->getApp('deq02uhb6');
+    print_r($resp);
     //修改app状态
-    // $mergePublishRtmp = null;
-    // $mergePublishRtmp['enable'] = true;
-    // $resp = $client->UpdateApp('deqq25wl9', $hub, $title, null, $mergePublishRtmp);
-    // print_r($resp);
+    $mergePublishRtmp = null;
+    $mergePublishRtmp['enable'] = true;
+    $resp = $client->UpdateApp('deqq25wl9', $hub, $title, null, $mergePublishRtmp);
+    print_r($resp);
     //删除app
-    // $resp = $client->deleteApp('deq02uhb6');
-    // print_r($resp);
+    $resp = $client->deleteApp('deq02uhb6');
+    print_r($resp);
     //获取房间连麦的成员
-    // $resp=$client->getappUserNum("deqq25wl9", 'lfx');
-    // print_r($resp);
+    $resp=$client->getappUserNum("deqq25wl9", 'lfx');
+    print_r($resp);
     //剔除房间的连麦成员
-    // $resp=$client->kickingPlayer("deqq25wl9", 'lfx', "qiniu-f6e07b78-4dc8-45fb-a701-a9e158abb8e6");
-    // print_r($resp);
+    $resp=$client->kickingPlayer("deqq25wl9", 'lfx', "qiniu-f6e07b78-4dc8-45fb-a701-a9e158abb8e6");
+    print_r($resp);
     // 列举房间
     $resp=$client->listRooms("deqq25wl9", '', '', '');
     print_r($resp);
     //鉴权的有效时间: 1个小时.
-    // $resp = $client->appToken("deq02uhb6", "lfx", '1111', (time()+3600), 'user');
-    // print_r($resp);
+    $resp = $client->appToken("deq02uhb6", "lfx", '1111', (time()+3600), 'user');
+    print_r($resp);
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
--- lib/Qiniu/Pili/AppClient.php
+++ lib/Qiniu/Pili/AppClient.php
@@ -17,8 +17,11 @@ class AppClient
     }
 
     /*
-     * ownerId: 要创建房间的所有者
-     * appId: 房间名称
+     * hub: 直播空间名
+     * title: app 的名称  注意，Title 不是唯一标识，重复 create 动作将生成多个 app
+     * NoAutoCloseRoom: bool 类型，可选，禁止自动关闭房间。默认为 false ，即用户退出房间后，房间会被主动清理释放。
+     * NoAutoCreateRoom: bool 类型，可选，禁止自动创建房间。默认为 false ，即不需要主动调用接口创建即可加入房间。
+     * NoAutoKickUser: bool 类型，可选，禁止自动踢人（抢流）。默认为 false ，即同一个身份的 client (app/room/user) ，新的连麦请求可以成功，旧连接被关闭。
      */
     public function createApp($hub, $title, $maxUsers = null, $noAutoCloseRoom = null, $noAutoCreateRoom = null, $noAutoKickUser = null)
     {
@@ -46,8 +49,21 @@ class AppClient
     }
 
     /*
-     * ownerId: 要创建房间的所有者
-     * appId: 房间名称
+     * appId: app 的唯一标识，创建的时候由系统生成。
+     * Title: app 的名称， 可选。
+     * Hub: 绑定的直播 hub，可选，用于合流后 rtmp 推流。
+     * MaxUsers: int 类型，可选，连麦房间支持的最大在线人数。
+     * NoAutoCloseRoom: bool 指针类型，可选，true 表示禁止自动关闭房间。
+     * NoAutoCreateRoom: bool 指针指型，可选，true 表示禁止自动创建房间。
+     * NoAutoKickUser: bool 类型，可选，禁止自动踢人。
+     * MergePublishRtmp: 连麦合流转推 RTMP 的配置，可选择。其详细配置包括如下
+            Enable: 布尔类型，用于开启和关闭所有房间的合流功能。
+            AudioOnly: 布尔类型，可选，指定是否只合成音频。
+            Height, Width: int64，可选，指定合流输出的高和宽，默认为 640 x 480。
+            OutputFps: int64，可选，指定合流输出的帧率，默认为 25 fps 。
+            OutputKbps: int64，可选，指定合流输出的码率，默认为 1000 。
+            URL: 合流后转推旁路直播的地址，可选，支持魔法变量配置按照连麦房间号生成不同的推流地址。如果是转推到七牛直播云，不建议使用该配置。
+            StreamTitle: 转推七牛直播云的流名，可选，支持魔法变量配置按照连麦房间号生成不同的流名。例如，配置 Hub 为 qn-zhibo ，配置 StreamTitle 为 $(roomName) ，则房间 meeting-001 的合流将会被转推到 rtmp://pili-publish.qn-zhibo.***.com/qn-zhibo/meeting-001地址。详细配置细则，请咨询七牛技术支持。
      */
     public function updateApp($appId, $hub, $title, $maxUsers = null, $mergePublishRtmp = null, $noAutoCloseRoom = null, $noAutoCreateRoom = null, $noAutoKickUser = null)
     {
@@ -79,7 +95,7 @@ class AppClient
     }
 
     /*
-     * appId: 房间名称
+     * appId: app 的唯一标识，创建的时候由系统生成。
      */
     public function getApp($appId)
     {
@@ -93,7 +109,7 @@ class AppClient
     }
 
     /*
-     * appId: 房间名称
+     * appId: app 的唯一标识，创建的时候由系统生成
      */
     public function deleteApp($appId)
     {
@@ -108,7 +124,8 @@ class AppClient
 
     /*
      * 获取房间的人数
-     * appId: 房间名称
+     * appId: app 的唯一标识，创建的时候由系统生成。
+     * roomName: 操作所查询的连麦房间。
      */
     public function getappUserNum($appId, $roomName)
     {
@@ -123,10 +140,11 @@ class AppClient
 
    /*
     * 踢出玩家
-    * appId: 房间名称
+    * appId: app 的唯一标识，创建的时候由系统生成。
+    * roomName: 连麦房间
     * userId: 请求加入房间的用户ID
     */
-    public function kickingPlayer($appId, $roomName, $UserId)
+    public function kickingPlayer($appId, $roomName, $userId)
     {
         $url = sprintf("%s/%s/rooms/%s/users/%s", $this->_baseURL, $appId, $roomName, $UserId);
         try {
@@ -139,7 +157,10 @@ class AppClient
 
     /*
      * 获取房间的人数
-     * appId: 房间名称
+     * appId: app 的唯一标识，创建的时候由系统生成。
+     * prefix: 所查询房间名的前缀索引，可以为空。
+     * offset: int 类型，分页查询的位移标记。
+     * limit: int 类型，此次查询的最大长度。
      */
     public function listRooms($appId, $prefix, $offset, $limit)
     {
@@ -153,10 +174,11 @@ class AppClient
     }
 
     /*
-     * appId: 房间名称
-     * userId: 请求加入房间的用户ID
-     * perm: 该用户的房间管理权限，"admin"或"user"，房间主播为"admin"，拥有将其他用户移除出房间等特权。
-     * expireAt: int64类型，鉴权的有效时间，传入秒为单位的64位Unix时间，token将在该时间后失效。
+     * appId: app 的唯一标识，创建的时候由系统生成。
+     * roomName: 房间名称，需满足规格 ^[a-zA-Z0-9_-]{3,64}$
+     * userId: 请求加入房间的用户 ID，需满足规格 ^[a-zA-Z0-9_-]{3,50}$
+     * expireAt: int64 类型，鉴权的有效时间，传入以秒为单位的64位Unix绝对时间，token 将在该时间后失效。
+     * permission: 该用户的房间管理权限，"admin" 或 "user"，默认为 "user" 。当权限角色为 "admin" 时，拥有将其他用户移除出房间等特权.
      */
     public function appToken($appId, $roomName, $userId, $expireAt, $permission)
     {
